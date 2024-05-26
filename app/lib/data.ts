980c39c5b4ae62c2e1975327addcd772232bffda
rikulahdeaho <riku.lahdeaho@outlook.com>
2024-05-26 10:51:27 +0300

    update

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -9,7 +9,6 @@ import {
   Revenue,
 } from './definitions';
 import { formatCurrency } from './utils';
-import { unstable_noStore as noStore } from 'next/cache';
 
 export async function fetchRevenue() {
   // Add noStore() here to prevent the response from being cached.
@@ -34,7 +33,6 @@ export async function fetchRevenue() {
 
 export async function fetchLatestInvoices() {
   try {
-    noStore();
     const data = await sql<LatestInvoiceRaw>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
       FROM invoices
@@ -55,7 +53,6 @@ export async function fetchLatestInvoices() {
 
 export async function fetchCardData() {
   try {
-    noStore();
     // You can probably combine these into a single SQL query
     // However, we are intentionally splitting them to demonstrate
     // how to initialize multiple queries in parallel with JS.
@@ -97,7 +94,6 @@ export async function fetchFilteredInvoices(
   const offset = (currentPage - 1) * ITEMS_PER_PAGE;
 
   try {
-    noStore();
     const invoices = await sql<InvoicesTable>`
       SELECT
         invoices.id,
@@ -128,7 +124,6 @@ export async function fetchFilteredInvoices(
 
 export async function fetchInvoicesPages(query: string) {
   try {
-    noStore();
     const count = await sql`SELECT COUNT(*)
     FROM invoices
     JOIN customers ON invoices.customer_id = customers.id
@@ -150,7 +145,6 @@ export async function fetchInvoicesPages(query: string) {
 
 export async function fetchInvoiceById(id: string) {
   try {
-    noStore();
     const data = await sql<InvoiceForm>`
       SELECT
         invoices.id,
@@ -176,7 +170,6 @@ export async function fetchInvoiceById(id: string) {
 
 export async function fetchCustomers() {
   try {
-    noStore();
     const data = await sql<CustomerField>`
       SELECT
         id,
@@ -195,7 +188,6 @@ export async function fetchCustomers() {
 
 export async function fetchFilteredCustomers(query: string) {
   try {
-    noStore();
     const data = await sql<CustomersTableType>`
 		SELECT
 		  customers.id,
